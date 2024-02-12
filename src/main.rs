5835d55e160343ef4f6f5941c56bd4da9815030b
commonkestrel <jettb6@gmail.com>
2024-02-11 22:52:19 -0800

    Actually run things

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use clap::{Parser, Subcommand};
 
 mod build;
-mod diagnostic;
+pub mod cfg;
+pub mod diagnostic;
 mod lsp;
 mod new;
+pub mod span;
 
-use build::BuildArgs;
-use lsp::LspArgs;
-use new::NewArgs;
+use build::{BuildArgs, BuildError};
+use lsp::{LspArgs, LspError};
+use new::{NewArgs, NewError};
+use thiserror::Error;
 
 #[derive(Debug, Parser)]
 #[clap(name = "fate", version)]
@@ -23,4 +26,24 @@ enum Command {
     Lsp(LspArgs),
 }
 
-fn main() {}
+#[derive(Debug, Error)]
+enum Error {
+    #[error(transparent)]
+    New(#[from] NewError),
+    #[error(transparent)]
+    Build(#[from] BuildError),
+    #[error(transparent)]
+    Lsp(#[from] LspError),
+}
+
+#[async_std::main]
+async fn main() -> Result<(), Error> {
+    let args = Args::parse();
+
+    Ok(match args.command {
+        Command::New(args) => new::new(args).await?,
+        Command::Build(args) => build::build(args).await?,
+        // Oh how I yearn for `!` to be stable
+        Command::Lsp(args) => lsp::start(args).await.map(|_| unreachable!())?,
+    })
+}
