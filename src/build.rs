18acb7263155b987fe182dd9ff028baafabd5db1
commonkestrel <jettb6@gmail.com>
2024-02-11 22:51:55 -0800

    Start on real build

--- src/build.rs
+++ src/build.rs
@@ -1,20 +1,69 @@
+use crate::cfg::{Config, ConfigError, ProjectType};
+use async_std::fs::File;
 use clap::Args;
+use std::path::PathBuf;
 use thiserror::Error;
 
 mod ascii;
 mod expr;
 mod generator;
-mod lexer;
-mod parse;
+pub mod lexer;
+pub mod token;
+mod parser;
 
-pub use parse::Span;
+const SOURCE: &str = "src";
 
 #[derive(Debug, Args)]
-pub struct BuildArgs {}
+pub struct BuildArgs {
+    /// A singular file to compile
+    file: Option<PathBuf>,
+    /// The home directory of the project (default to the current working directory)
+    #[clap(short = 'd', long, alias = "dir")]
+    home: Option<PathBuf>,
+}
 
 #[derive(Debug, Error)]
-pub enum BuildError {}
+pub enum BuildError {
+    #[error(transparent)]
+    Config(#[from] ConfigError),
+    #[error("unable to open root file")]
+    Root(std::io::Error),
+    #[error("error lexing file `{0}`")]
+    Lex(PathBuf),
+}
 
-pub fn build(args: BuildArgs) -> Result<(), BuildError> {
+pub async fn build(args: BuildArgs) -> Result<(), BuildError> {
+    let cfg = Config::parse(args.home.as_ref()).await?;
+
+    let root_file = match cfg.project.ty {
+        ProjectType::Exe => "main.fate",
+        ProjectType::Lib => "lib.fate",
+    };
+
+    let home_path = args
+        .home
+        .map(|dir| dir.join(SOURCE))
+        .unwrap_or(PathBuf::from(SOURCE));
+
+    let root_path = home_path.join(root_file);
+    let root = File::open(&root_path)
+        .await
+        .map_err(|err| BuildError::Root(err))?;
+
+    let root_stream = match lexer::lex(root_path.to_string_lossy().to_string(), root).await {
+        Ok(stream) => stream,
+        Err(errors) => {
+            for err in errors {
+                err.emit().await
+            }
+
+            return Err(BuildError::Lex(root_path));
+        }
+    };
+
+    if cfg!(debug_assertions) {
+        println!("{root_stream:#?}")
+    }
+
     Ok(())
 }
